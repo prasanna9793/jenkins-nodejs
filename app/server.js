c2d35e7563c7eca876e38b8370e71052b65013cf
prasanna9793 <prasanna.ramesh1324@gmail.com>
2026-05-22 00:17:15 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello from Node.js DevOps Projects3s 🚀");
+    res.send("Hello from Node.js DevOps Projects-2 🚀");
 });
 
 app.get('/health', (req, res) => {
